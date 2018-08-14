1cb2afba080f4ad61c654e1fabf3cfb53c34be4d
clinturbin <clinturbin@gmail.com>
2018-08-14 07:31:56 -0400

    Modified read write exercise solution to include readline to handle user input

--- read-and-write/cap_file_2.js
+++ read-and-write/cap_file_2.js
@@ -7,23 +7,35 @@
 //-----------------------------------------------
 
 var fs = require("fs");
+var readline = require("readline");
 
-var inputFile = "file1.txt";
-var outputFile = "cap.txt";
-
-fs.readFile(inputFile, "utf8", function (error, content) {
-    if (error) {
-        console.log(error.message);
-    }
-    else {
-        fs.writeFile(outputFile, content.toUpperCase(), function (error) {
-            if (error) {
-                console.log(error.message);
-            } else {
-                console.log("Input file: %s", inputFile);
-                console.log("Output file: %s", outputFile);
-                console.log("Wrote to file %s", outputFile);
-            }
-        })
-    }
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
+
+rl.question("Input File: : ", function (answer1) {
+    rl.question("Output File: ", function (answer2) {
+        readWrite(answer1, answer2);
+        rl.close();
+    })
+});
+
+var readWrite = function (inputFile, outputFile) {
+    fs.readFile(inputFile, "utf8", function (error, content) {
+        if (error) {
+            console.log(error.message);
+        }
+        else {
+            fs.writeFile(outputFile, content.toUpperCase(), function (error) {
+                if (error) {
+                    console.log(error.message);
+                } else {
+                    console.log("Input file: %s", inputFile);
+                    console.log("Output file: %s", outputFile);
+                    console.log("Wrote to file %s", outputFile);
+                }
+            })
+        }
+    });
+};
